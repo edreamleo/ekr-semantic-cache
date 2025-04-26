d81facfd99759356a205fb2f534d0338da6ae4c6
Edward K. Ream <edreamleo@gmail.com>
2025-04-26 05:03:33 -0500

    Add two files

---
+++ __init__.py
@@ -0,0 +1,4 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20250426050033.1: * @file __init__.py
+# ekr-semantic-cache/__init__.py
+#@-leo
---
+++ semantic_cacher.py
@@ -0,0 +1,11 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20250426050140.1: * @file semantic_cacher.py
+"""The main program for the semantic cacher"""
+
+#@+others
+#@-others
+
+
+#@@language python
+#@@tabwidth -4
+#@-leo
