04286fe5b82ef1b554dd0c15c0a1673890b1b6da
Edward K. Ream <edreamleo@gmail.com>
2025-04-26 05:06:05 -0500

    Rename

--- semantic_cache.py
+++ semantic_cache.py
@@ -1,5 +1,5 @@
 #@+leo-ver=5-thin
-#@+node:ekr.20250426050140.1: * @file semantic_cacher.py
+#@+node:ekr.20250426050140.1: * @file semantic_cache.py
 """The main program for the semantic cacher"""
 
 #@+others
