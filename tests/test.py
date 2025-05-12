31df980102562e37eec3797a11f70bdf337405a4
Edward K. Ream <edreamleo@gmail.com>
2025-05-12 07:21:14 -0500

    Reorg

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 #@+leo-ver=5-thin
-#@+node:ekr.20250512061658.1: * @file semantic_cache/tests/test.py
+#@+node:ekr.20250512061658.1: * @file tests/test.py
 """Unit tests the ekr-semantic-cache project"""
 from unittest import TestCase
 from leo.core import leoGlobals as g
